1b80da8a23e8bf02e4796f9e9b59239a1e3b644a
Laurence Sonnenberg <laurencesonnenberg01@gmail.com>
2022-11-01 11:48:58 +0200

    format code

--- server.go
+++ server.go
@@ -8,31 +8,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Defining the Graphql handler
 func graphqlHandler() gin.HandlerFunc {
-	// NewExecutableSchema and Config are in the generated.go file
-	// Resolver is in the resolver.go file
 	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
 	return func(c *gin.Context) {
 		h.ServeHTTP(c.Writer, c.Request)
 	}
 }
 
-// Defining the Playground handler
 func playgroundHandler() gin.HandlerFunc {
 	h := playground.Handler("GraphQL", "/query")
+
 	return func(c *gin.Context) {
 		h.ServeHTTP(c.Writer, c.Request)
 	}
 }
 
 func main() {
-	// Setting up Gin
 	r := gin.Default()
+
 	r.POST("/query", graphqlHandler())
 	r.GET("/", playgroundHandler())
-	err := r.Run()
-	if err != nil {
+
+	if err := r.Run(); err != nil {
 		panic("Server failed to start on port: " + "8080")
 	}
 }
